3ea383cfd347fa27fbbebefaf13841c0afecf338
Lucho <Lucho@Lucho>
2018-04-30 21:52:10 -0600

    clase habitacio con atributos

---
+++ src/hotelderafael/Habitacion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelderafael;
+
+/**
+ *
+ * @author Lucho
+ */
+public class Habitacion {
+    private int tipo;
+    private String npiso;
+    private double precio;
+    
+}
